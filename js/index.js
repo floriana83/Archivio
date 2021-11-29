377536206f7d3ab964eb45f4d41a359dcb983be7
Floriana Domianello <floriana.domianello@gmail.com>
2021-11-29 19:57:42 +0100

    fix aggiunta nuova chiave active

--- js/index.js
+++ js/index.js
@@ -5,8 +5,11 @@ const API = "https://jsonplaceholder.typicode.com/users";
 document.addEventListener("DOMContentLoaded", () => {
     fetch(API)
         .then((response) => response.json())
+        .then((data) => data.map((element) => {
+            element.active=true
+            return element
+        })) 
         .then((data) => List(data));
-        console.log(response)
 });
 
 const render = (container, content) => (container.innerHTML = content);
@@ -15,10 +18,11 @@ const List = (data) => {
     const elements = data
         .map((item) => `
         <li>
-            <h3>${item.name}</h3>
-            <p>${item.username}</p>
+            <h3>${item.name}</h3> <input type="checkbox" ${item.active ? "checked" : ""} /> 
+            <p>${item.username}</p>         
             <p><strong>Email:</strong> <a href="mailto:${item.email}">${item.email}</a></p>
             <p><strong>Phone:</strong> <a href="tel:${item.phone}">${item.phone}</a></p>
+
          </li>`)
         .join("");
 
